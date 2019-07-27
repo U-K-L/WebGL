90ef2af8a2a1677cd819d756288b70ad2f1b0bce
U-K-L <WilliamsNoah1720@gmail.com>
2019-07-26 23:27:10 -0400

    Change files locations

--- shaders/webgl.js
+++ shaders/webgl.js
@@ -8,7 +8,7 @@ if(!gl){
 function readFile(name){
     alert("hi")
     var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", "./shaders/"+name+"", false)
+    rawFile.open("GET", "./shaders/"+name+"", true)
     rawFile.onstatereadychange = function(){
         if(rawFile.readyState === 4){
             if(rawFile.status === 200 || rawFile.status == 0){
